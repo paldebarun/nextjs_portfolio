b604efa206942dd58e4b132c41fad5fa1d30e817
Debarun <paldebarun27@gmail.com>
2024-02-17 22:28:05 +0530

    the blackhole pos is fixed

--- components/main/Herosection.tsx
+++ components/main/Herosection.tsx
@@ -3,9 +3,9 @@ import HeroContent from '../sub/HeroContent'
 
 const Herosection = () => {
   return (
-    <div   className=' h-full w-full flex flex-col '>
+    <div   className=' h-full w-full gap-[50px] flex flex-col '>
        
-       <video  loop  autoPlay muted className=' rotate-180 absolute top-[-650px] xl:top-[-600px]  h-full w-full left-0 z-[1] object-cover'>
+       <video  loop  autoPlay muted className=' rotate-180 absolute top-[-300px] xl:top-[-340px]  h-full w-full left-0 z-[1] object-cover'>
         <source src='/blackhole.webm'  type='video/webm'/>
        </video>
        
